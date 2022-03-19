359fe31a99c69a114af791c3ff88ce5fa4a70059
ryann127 <ryann.goldberg127@gmail.com>
2022-03-18 20:23:07 -0600

    trying to reach a game over page to add highscores when questions end and not just when timer ends. Worked on timer a bit.

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,12 @@ var highscoreBtn = document.querySelector(".highscore-btn");
 var startScreen = document.querySelector(".start-screen");
 var startButton = document.querySelector(".start-button");
 var questionScreen = document.querySelector("#question-screen");
-var timerElement = document.querySelector(".timer-count"); //include this in html somehow
-var wordBlank = document.querySelector(".word-blank"); // include in html
+var timerElement = document.querySelector(".timer-count"); 
+var wordBlank = document.querySelector("#word-blank"); 
+var resetButton = document.querySelector(".reset-button")
+var winElement = document.querySelector("#win-game");
+var highscoreElement = document.querySelector("#highscore-title");
+
 
 var timerCount;
 var winCounter = 0;
@@ -16,11 +20,11 @@ var isWrong = false;
 var questions = [
     {
         name: "What does HTML stand for?",
-        answers: ["Hot Topic Men's leggings", "Hyper Text Markup Language", "Hyper Text Made-Up Language", "Hyper Text Markup Lingo"],
+        answers: ["Hot Topic Mens leggings", "Hyper Text Markup Language", "Hyper Text Made-Up Language", "Hyper Text Markup Lingo"],
         correct: "Hyper Text Markup Language",
     },
     {
-        name: "Where is the correct place to insera JavaScript",
+        name: "Where is the correct place to insert JavaScript",
         answers: ["Body Section","Head Section", "Both body and head sections will work"],
         correct: "Body Section",
     },
@@ -38,6 +42,21 @@ var questions = [
         name: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
         answers: ["if i <> 5", "if (i<>5)", "if i=! 5 then", "if (i!=5)"],
         correct: "if (i!=5)",
+    },
+    {
+        name: "What are the JavaScript data types?",
+        answers: ["Number", "Boolean", "String", "all of the above"], 
+        correct: "all of the above",
+    },
+    {
+        name: "What is an undeclared variable?",
+        answers: ["Variables that do not exist in a program", "Variables that are declared but with no value", "Non-existent", "Variable that returns and undefined value"],
+        correct: "Variables that do not exist in a program",
+    },
+    {
+        name: "Which is not looping structures in JavaScript?",
+        answers: ["for", "while", "do-while", "given"],
+        correct: "given",
     }
 ];
 
@@ -53,6 +72,8 @@ function startQuiz() {
     highscoreBtn.setAttribute("class", "hide");
     // display first question
 
+    isWin = false; 
+
     timerCount = 75; 
 
     questionDisplay();
@@ -61,51 +82,102 @@ function startQuiz() {
 
 
 
-// function startTimer(){
-//     console.log("timer");
-//     var timer = setInterval(function() {
-//         timerCount--; 
-//         timerElement.textContent = "Time Left: " + timerCount; 
-//         if (timerCount >= 0 && isCorrect) {
-//             rightAnswer();
-//         }
-//         if (isWrong) {
-//             timerCount = timerCount - 5;
-//             wordBlank.textContent = "Wrong!"
-//         } 
-//         if (timerCount === 0) {
-//             clearInterval(timer);
-//             gameOver();
-//         }
+function startTimer(){
+    console.log("timer");
+    var timer = setInterval(function() {
+        timerCount--; 
+        timerElement.textContent = "Time Left: " + timerCount; 
+        if (timerCount >= 0 && isCorrect) {
+            rightAnswer();
+        }
+        if (isWrong && timerCount >=5) {
+            timerCount = timerCount - 5;
+        } 
+        if (timerCount <= 0) {
+            clearInterval(timer);
+            gameOver();
+        }
 
-//     }, 1000);
+    }, 1000);
     
+}
+
+function rightAnswer() {
+    winCounter++;
+    startButton.disabled = false; // necessary?
+
+    getWins();
+}
+
+// function getWins() {
+
+//   var storedWins = localStorage.getItem("winCount"); 
+//   if (storedWins === null) {
+//     winCounter = 0;
+//   } else {
+//     winCounter = storedWins;
+//   }
+//   winElement.textContent = winCounter;
+
+
+
 // }
 
-// function rightAnswer() {
-//     wordBlank.textContent = "Correct!"
-//     winCounter++;
-//     startButton.disabled = false; // necessary?
+function gameOver() {
 
-//     setWins();
-// }
+    questionScreen.setAttribute("class", "hide");
+    timerElement.textContent("");
 
-// setWins() {
+    wordBlank.textContent = "Game Over!";
+    
+    var enterInitials = document.createElement("h1"); 
+    enterInitials.innerHTML = "Enter your initials";
 
+    var scoreForm = document.createElement("form");
 
+    var inputText = document.createElement("input");
+    inputText.setAttribute("type", "text");
 
-// }
+    var submitButton = document.createElement("button");
+    submitButton.setAttribute("class", "submit-button");
+    submitButton.innerHTML = "Submit";
+
+    scoreForm.appendChild(inputText);
+    scoreForm.appendChild(submitButton);
+
+    winElement.appendChild(enterInitials);
+    winElement.appendChild(scoreForm);
+
+function storeScore() {
+    localStorage.setItem("score", JSON.stringify(scoreList));
+    }
+
+scoreForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var initialText = inputText.value.trim();
+
+    if (initialText === "") {
+    return;
+    }
+
+    scoreList.push(initialText + "-" + timerCount);
+    inputText.value = "";
+
+    storeScore();
+    // highscoreList();
+  });
+};
 
 // create a function that displays questions
 function questionDisplay() {
-    console.log("hello");
+    
     // create a template to inject questions into
     var template = `
         <h2> ${questions[currentQuestion].name} </h2>
-        <button class= "answer-button" value= '${questions[currentQuestion].answers[0]}'> ${questions[currentQuestion].answers[0]} </button>
-        <button class= "answer-button" value= '${questions[currentQuestion].answers[1]}'> ${questions[currentQuestion].answers[1]} </button>
-        <button class= "answer-button" value= '${questions[currentQuestion].answers[2]}'> ${questions[currentQuestion].answers[2]} </button>
-        <button class= "answer-button" value= '${questions[currentQuestion].answers[3]}'> ${questions[currentQuestion].answers[3]} </button>
+        <button id="first-button" class="answer-button" value='${questions[currentQuestion].answers[0]}'> ${questions[currentQuestion].answers[0]} </button>
+        <button class="answer-button" value='${questions[currentQuestion].answers[1]}'> ${questions[currentQuestion].answers[1]} </button>
+        <button class="answer-button" value='${questions[currentQuestion].answers[2]}'> ${questions[currentQuestion].answers[2]} </button>
+        <button class="answer-button" value='${questions[currentQuestion].answers[3]}'> ${questions[currentQuestion].answers[3]} </button>
     `
     // use inner html method to inject template into question div
     questionScreen.innerHTML = template;
@@ -114,29 +186,34 @@ function questionDisplay() {
 // read the docs on the event interface
 
 function checkAnswer(event) {
-    console.log(event.target.innerText);
+    event.preventDefault();
+    // console.log(event.target.innerText);
     // create variable for event.target.innerText to compare to right answer
     var choice = event.target;
     var answerChosen = event.target.value;
+    console.log(answerChosen);
 
     if (!choice.classList.contains("answer-button")) {
         return;
     }
-    if (answerChosen === questions.correct) {
+
+    if (answerChosen === questions[currentQuestion].correct) {
         isCorrect = true; 
-    }
-    
+        wordBlank.textContent = "Correct!";
+    } else {
+        isWrong = true; 
+        wordBlank.textContent = "Wrong!";
+    };
 
-
-    // if (answers[i] === correct){ 
-    //     wordBlank.textContent = "you are Correct!";
-    // }
     //increment currentQuestion by 1
     currentQuestion++;
     questionDisplay();
+    
     
 }
 //attach event listener to start button
 startButton.addEventListener("click", startQuiz);
 // attach event listener to entire doc to check answers
 document.addEventListener("click", checkAnswer);
+// attach event listener to reset highscore
+// resetButton.addEventListener("click", winCounter=0);
